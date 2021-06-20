01c0eed949de1f3caf299826158784a7d51779f4
AndresMC <aemc_1999@hotmail.com>
2021-06-20 01:32:36 -0500

    Secure asset

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,18 +7,18 @@
 	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
 	<!-- VENDOR CSS -->
-	<link rel="stylesheet" href="{{ asset('vendor') }}/bootstrap/css/bootstrap.min.css">
-	<link rel="stylesheet" href="{{ asset('vendor') }}/font-awesome/css/font-awesome.min.css">
-	<link rel="stylesheet" href="{{ asset('vendor') }}/linearicons/style.css">
+	<link rel="stylesheet" href="{{ secure_asset('vendor') }}/bootstrap/css/bootstrap.min.css">
+	<link rel="stylesheet" href="{{ secure_asset('vendor') }}/font-awesome/css/font-awesome.min.css">
+	<link rel="stylesheet" href="{{ secure_asset('vendor') }}/linearicons/style.css">
 	<!-- MAIN CSS -->
-	<link rel="stylesheet" href="{{ asset('css') }}/main.css">
+	<link rel="stylesheet" href="{{ secure_asset('css') }}/main.css">
 	<!-- FOR DEMO PURPOSES ONLY. You should remove this in your project -->
-	<link rel="stylesheet" href="{{ asset('css') }}/demo.css">
+	<link rel="stylesheet" href="{{ secure_asset('css') }}/demo.css">
 	<!-- GOOGLE FONTS -->
 	<link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700" rel="stylesheet">
 	<!-- ICONS -->
-	<link rel="apple-touch-icon" sizes="76x76" href="{{ asset('img') }}/apple-icon.png">
-	<link rel="icon" type="image/png" sizes="96x96" href="{{ asset('img') }}/favicon.png">
+	<link rel="apple-touch-icon" sizes="76x76" href="{{ secure_asset('img') }}/apple-icon.png">
+	<link rel="icon" type="image/png" sizes="96x96" href="{{ secure_asset('img') }}/favicon.png">
 	<link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/highlight.js/9.12.0/styles/default.min.css">
 	<script src="//cdnjs.cloudflare.com/ajax/libs/highlight.js/9.12.0/highlight.min.js"></script>
 </head>
@@ -719,10 +719,10 @@
 	  });
 	});
 	</script>
-	<script src="{{ asset('vendor') }}/jquery/jquery.min.js"></script>
-	<script src="{{ asset('vendor') }}/bootstrap/js/bootstrap.min.js"></script>
-	<script src="{{ asset('vendor') }}/jquery-slimscroll/jquery.slimscroll.min.js"></script>
-	<script src="{{ asset('scripts') }}/klorofil-common.js"></script>
+	<script src="{{ secure_asset('vendor') }}/jquery/jquery.min.js"></script>
+	<script src="{{ secure_asset('vendor') }}/bootstrap/js/bootstrap.min.js"></script>
+	<script src="{{ secure_asset('vendor') }}/jquery-slimscroll/jquery.slimscroll.min.js"></script>
+	<script src="{{ secure_asset('scripts') }}/klorofil-common.js"></script>
 </body>
 
 </html>
